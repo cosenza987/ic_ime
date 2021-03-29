93b97eac5ea75550926d92876946043f70ffbaf0
cosenza987 <80402245+cosenza987@users.noreply.github.com>
2021-03-28 21:21:53 -0300

    Update VE2020Q2.c

--- VE 2020 CFG/VE2020Q2.c
+++ VE 2020 CFG/VE2020Q2.c
@@ -3,43 +3,42 @@
 
 int main() {
     int alt, tri, base, base1, base2, entra, row;
-    printf("Digite a alutra do triforce: ");
+    printf("Digite a altura do triforce: ");
     scanf(" %d", &alt);
     if(alt%2 != 0 || alt < 4) {
         printf("ALTURA INVALIDA\n");
     } else {
         row = 0;
-        tri = (alt / 2) + 1; //pegando a altura de cada triangulo
+        tri = (alt / 2); //pegando a altura de cada triangulo
         base = (2*tri) - 1;
-        base1 = base;
-        base2 = base;
-        entra = tri - 1;
+        base1 = base; // base do triangulo de cima
+        base2 = base; //base do triangulo invisivel de baixo
+        entra = tri - 1; //a quantidade de espacos antes do primeiro triangulo da parte de baixo
         //imprimindo o triangulo de cima
         for(int j = 0; j < tri; j++) {
-            for(int i = 0; i < base1 - 1; i++) {
+            for(int i = 0; i < base1; i++) {
             printf(" ");
             }
             base1--;
-            for(int k = 0; k < (2*row) - 1; k++) {
+            for(int k = 0; k < (2*row) + 1; k++) {
                 printf("*");
             }
-            if(j != tri - 1) {
-                printf("\n");
-            }
+            printf("\n");
             row++;
         }
+        //imprimindo os dois de baixo agora
         row = 0;
         for(int j = 0; j < tri; j++) {
             for(int i = 0; i < entra; i++) {
                 printf(" ");
             }
-            for(int i = 0; i < (2*row) - 1; i++) {
+            for(int i = 0; i < (2*row) + 1; i++) {
                 printf("*");
             }
             for(int i = 0; i < base2; i++) {
                 printf(" ");
             }
-            for(int i = 0; i < (2*row) - 1; i++) {
+            for(int i = 0; i < (2*row) + 1; i++) {
                 printf("*");
             }
             entra--;
